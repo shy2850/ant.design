55c461603444f153d2025b2f010e2c23d8cabba3
shiyangyang <shiyangyang@baidu.com>
2016-06-28 22:28:27 +0800

    init

---
+++ f2e-conf.js
@@ -0,0 +1,24 @@
+exports.localhost = {
+    root: __dirname,
+    welcome: 'index.html',
+    port: 8899,
+    cdn: true,
+    filter: {
+        get: function (req, resp) {
+            if (req.url.match(/^[^\.]+$/)) {
+                req.url = '/ant-design/index.html';
+            }
+        }
+    },
+    agent: {
+        get: function (pathname) {
+            return {
+                host: '127.0.0.1',
+                port: 8899,
+                path: function (url) {
+                    return '/ant-design' + url.path;
+                }
+            };
+        }
+    }
+};
